8fb57e97c2be1d2c24d6bf46cce7685095294572
pivnati <natanmuleta911@gmail.com>
2024-08-18 07:44:47 -0700

    inside the auth-bloc file we make the usersignup private

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,18 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/physics.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_full_structure/features/auth/domain/usecases/user_sign_up.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final UserSignUp _userSignUp;
+  AuthBloc({
+    required UserSignUp userSignUp,
+  }) :_userSignUp=userSignUp
+  , super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) => {});
   }
 }
