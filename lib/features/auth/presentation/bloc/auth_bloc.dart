15ff7790fb6c502fa6ab27897305694fee3816e3
pivnati <natanmuleta911@gmail.com>
2024-08-18 07:53:35 -0700

    create the AuthSignUp in the block and make it response

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/physics.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_full_structure/features/auth/domain/usecases/user_sign_up.dart';
 
@@ -8,11 +7,17 @@ part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  // ignore: unused_field
   final UserSignUp _userSignUp;
   AuthBloc({
     required UserSignUp userSignUp,
   }) :_userSignUp=userSignUp
   , super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) => {});
+    on<AuthSignUp>((event, emit) async{
+     final res=await _userSignUp(UserSignUpParams(
+        email: event.email,
+       password: event.password, 
+       name: event.name));
+    });
   }
 }
